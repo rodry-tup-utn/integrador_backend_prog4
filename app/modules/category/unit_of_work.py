8803ec5ceab89649cafc3b8d98df99240aba0d2f
rodry <rodrymza@gmail.com>
2026-04-21 12:29:32 -0300

    fix: arreglo en nombre de uow

--- app/modules/category/unit_of_work.py
+++ app/modules/category/unit_of_work.py
@@ -3,7 +3,7 @@ from app.core.unit_of_work import UnitOfWork
 from app.modules.category.repository import CategoryRepository
 
 
-class CategoriUnitOfWork(UnitOfWork["CategoriUnitOfWork"]):
+class CategoryUnitOfWork(UnitOfWork["CategoryUnitOfWork"]):
     def __init__(self, session: Session) -> None:
         super().__init__(session)
         self.categorias = CategoryRepository(session)
