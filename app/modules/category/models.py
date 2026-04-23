5e25581b9b2916b04b084d44aed80dc054740638
rodry <rodrymza@gmail.com>
2026-04-22 22:28:53 -0300

    fix: tipos de datos None y str

--- app/modules/category/models.py
+++ app/modules/category/models.py
@@ -5,8 +5,8 @@ from datetime import datetime, timezone
 class Category(SQLModel, table=True):
     """Tabla de categoria"""
 
-    id: int = Field(default=None, primary_key=True)
-    parent_id: int = Field(default=None, foreign_key="category.id")
+    id: int | None = Field(default=None, primary_key=True)
+    parent_id: int | None = Field(default=None, foreign_key="category.id")
     name: str = Field(max_length=50, min_length=4, unique=True, index=True)
     description: str | None = Field(default=None, min_length=5, max_length=255)
     image_url: str | None = Field(255)
