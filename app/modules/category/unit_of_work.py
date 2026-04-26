9ef4cc6b9cc4d4a2c375433fdda6d9750b67f112
rodry <rodrymza@gmail.com>
2026-04-26 19:03:22 -0300

    feature: agregado repositorio category_product link

--- app/modules/category/unit_of_work.py
+++ app/modules/category/unit_of_work.py
@@ -1,9 +1,11 @@
 from sqlmodel import Session
 from app.core.unit_of_work import UnitOfWork
 from app.modules.category.repository import CategoryRepository
+from app.modules.product_category.repository import ProductCategoryLinkRepository
 
 
 class CategoryUnitOfWork(UnitOfWork["CategoryUnitOfWork"]):
     def __init__(self, session: Session) -> None:
         super().__init__(session)
         self.categories = CategoryRepository(session)
+        self.category_products = ProductCategoryLinkRepository(session)
