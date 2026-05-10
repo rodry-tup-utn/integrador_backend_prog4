b624560601e4cd918ccfd60c9eb33ae48d2407cd
rodry <rodrymza@gmail.com>
2026-05-09 22:47:08 -0300

    feat: mejora de seguridad, se busca usuario en db, erroes segun 403, 401

--- app/modules/auth/dependencies.py
+++ app/modules/auth/dependencies.py
@@ -11,7 +11,12 @@ from app.modules.user.schemas import UserPrivate
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
-credentials_exception = HTTPException(
+forbidden_exception = HTTPException(
+    status_code=status.HTTP_403_FORBIDDEN,
+    detail="No tienes permisos para ejecutar esta operacion",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+unauthorized_exception = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
     detail="No se pudieron validar las credenciales",
     headers={"WWW-Authenticate": "Bearer"},
@@ -25,11 +30,6 @@ def get_user_service(session: Session = Depends(get_session)) -> UserService:
 def get_token_payload(
     token: str = Depends(oauth2_scheme),
 ) -> UserTokenData:
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="No se pudieron validar las credenciales",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
 
     try:
         payload = jwt.decode(
@@ -40,19 +40,22 @@ def get_token_payload(
         name = payload.get("name")
 
         if user_id is None or role is None or name is None:
-            raise credentials_exception
+            raise unauthorized_exception
 
         return UserTokenData(id=int(user_id), role=role, name=name)
     except jwt.PyJWTError:
-        raise credentials_exception
+        raise unauthorized_exception
 
 
 def get_current_user(
     token_data: UserTokenData = Depends(get_token_payload),
     svc: UserService = Depends(get_user_service),
 ):
-    user = svc.get_active_by_id(token_data.id)
-    return user
+    try:
+        user = svc.get_active_by_id(token_data.id)
+        return user
+    except HTTPException:
+        raise unauthorized_exception
 
 
 def get_current_admin_user(
@@ -62,8 +65,8 @@ def get_current_admin_user(
     try:
         user = svc.get_active_by_id(current_user.id)
         if user.role != UserModel.Role.ADMIN:
-            raise credentials_exception
+            raise forbidden_exception
     except Exception:
-        raise credentials_exception
+        raise forbidden_exception
 
     return user
