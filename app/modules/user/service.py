f30174e6cd89f525de5bf2f188fb79bba7f24aee
rodry <rodrymza@gmail.com>
2026-05-16 03:23:25 -0300

    feat: asignar y eliminar rol

--- app/modules/user/service.py
+++ app/modules/user/service.py
@@ -9,9 +9,9 @@ from app.modules.user.schemas import (
 )
 from sqlmodel import Session
 from app.modules.user.unit_of_work import UserUnitOfWork
-from app.modules.user.models import User, Role
+from app.modules.user.models import User, Role, UserRoleLink
 from datetime import datetime, timezone
-from app.core.security import verify_password, get_password_hash
+from app.core.security import get_password_hash
 
 
 class UserService:
@@ -43,6 +43,12 @@ class UserService:
                 f"Ya existe un usuario registrado con el email {email}",
             )
 
+    def _get_role_by_code_or_404(self, uow: UserUnitOfWork, code: str) -> Role:
+        role = uow.roles.get_by_code(code)
+        if not role:
+            raise HTTPException(status.HTTP_404_NOT_FOUND, f"Rol {code} inexistente")
+        return role
+
     def create(self, data: UserCreate) -> UserBase:
         with UserUnitOfWork(self._session) as uow:
             self._assert_email_unique(uow, data.email)
@@ -67,16 +73,6 @@ class UserService:
             for field, value in update_data.items():
                 setattr(user, field, value)
 
-            user.updated_at = datetime.now(timezone.utc)
-            uow.users.add(user)
-            result = UserPrivate.model_validate(user)
-
-        return result
-
-    def update_role(self, user_id: int, new_role: Role) -> UserPrivate:
-        with UserUnitOfWork(self._session) as uow:
-            user = self._get_active_or_404(uow, user_id)
-            user.role = new_role
             user.updated_at = datetime.now(timezone.utc)
             uow.users.add(user)
             result = UserPrivate.model_validate(user)
@@ -142,3 +138,60 @@ class UserService:
 
             result = UserList(data=data, total=total)
         return result
+
+    def asign_role(self, user_id: int, role_code: str, user_assing_id: int):
+        with UserUnitOfWork(self._session) as uow:
+            user = self._get_active_or_404(uow, user_id)
+            role = self._get_role_by_code_or_404(uow, role_code)
+
+            # verificacion de seguridad para usuario que asigna
+            self._get_active_or_404(uow, user_assing_id)
+
+            existing_link = uow.user_role.get_by_user_id_and_role_code(
+                user_id, role_code
+            )
+            created_time = datetime.now(timezone.utc)
+            # verificar si la relacion ya existe, y reactivar en caso de rol inactivos
+            if existing_link:
+
+                if existing_link.expires_at is None:
+                    return existing_link
+
+                existing_link.expires_at = None
+
+                uow.user_role.add(existing_link)
+                return existing_link
+
+            user_role = UserRoleLink(
+                user_id=user.id,  # type: ignore
+                role_code=role.code,
+                assigned_by_id=user_assing_id,
+                created_at=created_time,
+            )
+
+            uow.user_role.add(user_role)
+
+            return user_role
+
+    def revoke_role(self, user_id: int, role_code: str) -> None:
+        with UserUnitOfWork(self._session) as uow:
+            self._get_role_by_code_or_404(uow, role_code)
+            self._get_active_or_404(uow, user_id)
+
+            user_role_link = uow.user_role.get_by_user_id_and_role_code(
+                user_id, role_code
+            )
+            if not user_role_link:
+                raise HTTPException(
+                    status.HTTP_404_NOT_FOUND, "Relacion de rol no encontrada"
+                )
+
+            if user_role_link.expires_at is not None:
+                raise HTTPException(
+                    status.http_400, "El rol ya se encuentra desactivado"
+                )
+
+            actual_date = datetime.now(timezone.utc)
+            user_role_link.expires_at = actual_date
+
+            uow.user_role.add(user_role_link)
