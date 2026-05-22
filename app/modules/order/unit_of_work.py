4fe8ab8d51ce40e0e159e7be5d37ea771ec3394f
rodry <rodrymza@gmail.com>
2026-05-22 02:25:17 -0300

    feat: agregados repositorios necesarios al uow de ordenes

--- app/modules/order/unit_of_work.py
+++ app/modules/order/unit_of_work.py
@@ -1,10 +1,17 @@
 from app.core.unit_of_work import UnitOfWork
 from sqlmodel import Session
 from app.modules.order.repositories.order_repository import OrderRepository
-from app.modules.order.repositories.order_historial_repository import OrderHistorialRepository
+from app.modules.order.repositories.order_historial_repository import (
+    OrderHistorialRepository,
+)
 from app.modules.order.repositories.state_order_repository import StateOrderRepository
-from app.modules.order.repositories.payload_method_repository import PayloadMethodRepository
+from app.modules.order.repositories.payload_method_repository import (
+    PaymentMethodRepository,
+)
 from app.modules.order_item.repository import OrderItemRepository
+from app.modules.product.repository import ProductRepository
+from app.modules.product_ingredient.repository import ProductIngredientRepository
+from app.modules.user.repositories.address_repository import AddressRepository
 
 
 class OrderUnitOfWork(UnitOfWork["OrderUnitOfWork"]):
@@ -14,4 +21,7 @@ class OrderUnitOfWork(UnitOfWork["OrderUnitOfWork"]):
         self.historials = OrderHistorialRepository(session)
         self.order_items = OrderItemRepository(session)
         self.states = StateOrderRepository(session)
-        self.payload_methods = PayloadMethodRepository(session)
+        self.payment_methods = PaymentMethodRepository(session)
+        self.products = ProductRepository(session)
+        self.product_ingredients = ProductIngredientRepository(session)
+        self.addresses = AddressRepository(session)
